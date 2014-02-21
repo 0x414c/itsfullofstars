7b3b5586a0cbcdc7df6b438b03fcda2789daf6e7
Alexey Gorishny <0x6675636b@gmail.com>
2014-02-21 19:57:12 +0400

    Added handling of some screensaver-specific command-line arguments.

--- WindowsGame1/WindowsGame1/Program.cs
+++ WindowsGame1/WindowsGame1/Program.cs
@@ -1,13 +1,28 @@
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WindowsGame1 {
 #if WINDOWS || XBOX
-    static class Program {
+    internal static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         private static void Main(string[] args) {
+            if (args.Length > 0) {
+                switch (args[0]) {
+                    case "/s":
+                        Start();
+                        break;
+                }
+            }
+            else {
+                Start();
+            }
+        }
+
+        static void Start() {
             using (var game = new Game1()) {
                 game.Run();
             }
